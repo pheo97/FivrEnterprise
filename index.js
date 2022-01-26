08a718d8ee6f4fe8d76f497159278350be10f69d
Alupheo Msumba <juniormsumba@gmail.com>
2022-01-26 14:58:27 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const ejsMate = require('ejs-mate')
+const path = require('path')
+
+app.set('view engine','ejs');
+app.engine('ejs',ejsMate);
+app.set('views', path.join(__dirname,'views'))
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get("/", (req,res) =>{
+    res.render('home')
+})
+
+app.get("/properties", (req,res) =>{
+    res.render('properties')
+})
+app.get("/contact" , (req,res) =>{
+    res.render('contact')
+})
+
+app.listen(3000, ()=>{
+    console.log("listening on port 3000")
+})
